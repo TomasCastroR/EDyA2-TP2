7786db482670095949ba0883db1202ec4bceea05
Tomas Castro Rojas <tomascastrorojas18@gmail.com>
2021-06-05 16:52:43 -0300

    instancia de secuencias con arreglos paralelos

--- ArrSeq.hs
+++ ArrSeq.hs
@@ -2,3 +2,22 @@ module ArrSeq where
 
 import Seq
 import Par
+import qualified Arr as A
+ 
+instance Seq A.Arr where
+  emptyS = empty
+  singletonS = singletonS_
+  lengthS = lengthS_
+  nthS = nthS_
+  tabulateS = tabulateS_
+  mapS = mapS_
+  filterS = filterS_
+  appendS = appendS_
+  takeS = takeS_
+  dropS = dropS_
+  showtS = showtS_
+  showlS = showlS_
+  joinS = joinS_
+  reduceS = reduceS_
+  scanS = scanS_
+  fromList = fromList_
