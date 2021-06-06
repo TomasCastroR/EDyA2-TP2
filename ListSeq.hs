0b3d24ebc7865e8f09f70fc1766c1728a1d0d0ba
Tomas Castro Rojas <tomascastrorojas18@gmail.com>
2021-06-06 13:12:33 -0300

    sufijo List para todas las funciones

--- ListSeq.hs
+++ ListSeq.hs
@@ -4,79 +4,79 @@ import Seq
 import Par
 
 instance Seq [] where
-  emptyS = emptyS_
-  singletonS = singletonS_
-  lengthS = lengthS_
-  nthS = nthS_
-  tabulateS = tabulateS_
-  mapS = mapS_
-  filterS = filterS_
-  appendS = appendS_
-  takeS = takeS_
-  dropS = dropS_
-  showtS = showtS_
-  showlS = showlS_
-  joinS = joinS_
-  reduceS = reduceS_
-  scanS = scanS_
+  emptyS = emptyList
+  singletonS = singletonList
+  lengthS = lenghtList
+  nthS = nthList
+  tabulateS = tabulateList
+  mapS = mapList
+  filterS = filterList
+  appendS = appendList
+  takeS = takeList
+  dropS = dropList
+  showtS = showtList
+  showlS = showlList
+  joinS = joinList
+  reduceS = reduceList
+  scanS = scanList
   fromList = fromList_
 
-emptyS_ = []
+emptyList = []
 
-singletonS_ a = [a]
+singletonList a = [a]
 
-lengthS_ s = length s
+lenghtList s = length s
 
-nthS_ s n = s !! n
+nthList s n = s !! n
  
-tabulateS_ f n = tabulateS' f n 0 
+tabulateList f n = tabulateList' f n 0 
                  where 
-                  tabulateS' f 0 _ = emptyS_ 
-                  tabulateS' f n i = let (x, xs) = f i ||| tabulateS' f (n-1) (i+1)
+                  tabulateList' f 0 _ = emptyList 
+                  tabulateList' f n i = let (x, xs) = f i ||| tabulateList' f (n-1) (i+1)
                                     in x:xs
   
 
-mapS_ f [] = emptyS_ 
-mapS_ f (x:xs) = let (y, ys) = f x ||| mapS_ f xs
+mapList f [] = emptyList 
+mapList f (x:xs) = let (y, ys) = f x ||| mapList f xs
                  in y:ys
 
-filterS_ _ [] = emptyS_ 
-filterS_ p (x:xs) = let (y, ys) = p x ||| filterS_ p xs
+filterList _ [] = emptyList 
+filterList p (x:xs) = let (y, ys) = p x ||| filterList p xs
                     in if y then x : ys else ys 
 
-appendS_ [] sb = sb
-appendS_ sa [] = sa
-appendS_ (a:sa) sb = a: (appendS_ sa sb)
+appendList [] sb = sb
+appendList sa [] = sa
+appendList (a:sa) sb = a: (appendList sa sb)
 
-takeS_ s n = take n s
+takeList s n = take n s
 
-dropS_ s n = drop n s
+dropList s n = drop n s
 
-showtS_ [] = EMPTY
-showtS_ [x] = ELT x
-showtS_ xs = let largo = div (lengthS_ xs) 2
-                 (l, r) = (takeS_ xs largo) ||| (dropS_ xs largo)
+showtList [] = EMPTY
+showtList [x] = ELT x
+showtList xs = let largo = div (lenghtList xs) 2
+                   (l, r) = (takeList xs largo) ||| (dropList xs largo)
              in NODE l r
 
-showlS_ [] = NIL
-showlS_ (x:xs) = CONS x xs
+showlList [] = NIL
+showlList (x:xs) = CONS x xs
 
-joinS_ [] = emptyS_ 
-joinS_ (x:xs) = appendS_ x (joinS_ xs)
+joinList [] = emptyList 
+joinList (x:xs) = appendList x (joinList xs)
 
 contraer :: (a -> a -> a) -> [a] -> [a]
-contraer _ [] = emptyS_
-contraer _ [x] = singletonS_ x
+contraer _ [] = emptyList
+contraer _ [x] = singletonList x
 contraer f (x:y:xs) = let (z,zs) = f x y ||| contraer f xs
                       in z:zs
 
-reduceS_ _ e [] = e
-reduceS_ f e [x] = f e x
-reduceS_ f e xs = reduceS_ f e (contraer f xs)
+reduceList _ e [] = e
+reduceList f e [x] = f e x
+reduceList f e xs = reduceList f e (contraer f xs)
 
-scanS_ _ e [] = (emptyS_ , e)
-scanS_ f e [x] = (singletonS_ e, f e x)
-scanS_ f e xs = let (ys, r) = scanS_ f e (contraer f xs)
+scanList _ e [] = (emptyList , e)
+scanList f e [x] = (singletonList e, f e x)
+scanList f e xs = let (ys, r) = scanList f e (contraer f xs)
                 in (expandirSeq f xs ys, r) 
                 where
                   expandirSeq _ [] _ = []
