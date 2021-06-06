d60f3f9cd231b602c626f88a6917fd69bcd3e6c8
Tomas Castro Rojas <tomascastrorojas18@gmail.com>
2021-06-06 18:05:19 -0300

    Implementado todo excepto scan. Tal vez la implementacion de lo hecho es correcta pero no podes testear porque tira error

--- ArrSeq.hs
+++ ArrSeq.hs
@@ -3,21 +3,54 @@ module ArrSeq where
 import Seq
 import Par
 import qualified Arr as A
- 
+import Arr ((!))
+
 instance Seq A.Arr where
-  emptyS = empty
-  singletonS = singletonS_
-  lengthS = lengthS_
-  nthS = nthS_
-  tabulateS = tabulateS_
-  mapS = mapS_
-  filterS = filterS_
-  appendS = appendS_
-  takeS = takeS_
-  dropS = dropS_
-  showtS = showtS_
-  showlS = showlS_
-  joinS = joinS_
-  reduceS = reduceS_
-  scanS = scanS_
-  fromList = fromList_
+  emptyS = A.empty
+  singletonS = singletonArr
+  lengthS = A.length
+  nthS = (!)
+  tabulateS = A.tabulate
+  mapS = mapArr
+  filterS = filterArr
+  appendS = appendArr
+  takeS = takeArr
+  dropS = dropArr
+  showtS = showtArr
+  showlS = showlArr
+  joinS = A.flatten
+  reduceS = reduceArr
+  --scanS = scanArr
+  fromList = A.fromList
+
+singletonArr x = fromList [x]
+
+mapArr f arr = let n = lengthS arr 
+               in  tabulateS (\i -> (f (nthS arr i))) n
+
+filterArr p arr = joinS (mapArr (\x -> if p x then singletonArr x else emptyS) arr)
+
+appendArr arr brr = let n = lengthS arr
+                        m = lengthS brr
+                    in tabulateS (\i -> if i < n then nthS arr i else nthS brr i) (n+m)
+
+takeArr arr n = let l = lengthS arr
+                in if l < n then arr else A.subArray 0 n arr
+
+dropArr arr n = let l = lengthS arr
+                in if l < n then emptyS else A.subArray n (l-n) arr
+
+showtArr arr | largo == 0 = EMPTY
+             | largo == 1 = ELT (nthS arr 0)
+             | otherwise = NODE (takeArr arr (div largo 2)) (dropArr arr (div largo 2))
+            where largo = lengthS arr
+
+showlArr arr  | largo == 0 = NIL
+              | otherwise = CONS (nthS arr 0) (dropArr arr 1)
+              where largo = lengthS arr
+
+reduceArr f e arr | len == 0  = e
+                  | len == 1  = f e (nthS arr 0)
+                  | otherwise = let contraer = tabulateS (\i -> f (nthS arr (2*i)) (nthS arr ((2*i)+1))) (div len 2)
+                                in reduceArr f e contraer
+                  where len = lengthS arr
