e5ecbe17a5e5f09c1a67c7b4557481655e89e648
Tomas Castro Rojas <tomascastrorojas18@gmail.com>
2021-06-07 12:19:19 -0300

    variable largo en todo el programa. Esqueleto de scanArr, falta definir expandirArr

--- ArrSeq.hs
+++ ArrSeq.hs
@@ -37,8 +37,8 @@ joinArr = A.flatten
 
 fromListArr = A.fromList
 
-mapArr f arr = let n = lengthArr arr 
-               in  tabulateArr (\i -> (f (nthArr arr i))) n
+mapArr f arr = let largo = lengthArr arr 
+               in  tabulateArr (\i -> (f (nthArr arr i))) largo
 
 filterArr p arr = joinArr (mapArr (\x -> if p x then singletonArr x else emptyArr) arr)
 
@@ -46,27 +46,36 @@ appendArr arr brr = let n = lengthArr arr
                         m = lengthArr brr
                     in tabulateArr (\i -> if i < n then nthArr arr i else nthArr brr i) (n+m)
 
-takeArr arr n = let l = lengthArr arr
-                in if l < n then arr else A.subArray 0 n arr
+takeArr arr n = let largo = lengthArr arr
+                in if largo < n then arr else A.subArray 0 n arr
 
-dropArr arr n = let l = lengthArr arr
-                in if l < n then emptyArr else A.subArray n (l-n) arr
+dropArr arr n = let largo = lengthArr arr
+                in if largo < n then emptyArr else A.subArray n (largo-n) arr
 
 showtArr arr | largo == 0 = EMPTY
              | largo == 1 = ELT (nthArr arr 0)
              | otherwise = NODE (takeArr arr (div largo 2)) (dropArr arr (div largo 2))
-            where largo = lengthArr arr
+             where largo = lengthArr arr
 
 showlArr arr  | largo == 0 = NIL
               | otherwise = CONS (nthArr arr 0) (dropArr arr 1)
               where largo = lengthArr arr
 
-contraer f arr len | even len  = tabulateArr (\i -> f (nthArr arr (2*i)) (nthArr arr ((2*i)+1))) rango
-                   | otherwise = tabulateArr (\i -> if i == rango then nthArr arr (2*i) else f (nthArr arr (2*i)) (nthArr arr ((2*i)+1))) (rango+1)
-                   where rango = (div len 2)
+contraer f arr largo | even largo  = tabulateArr (\i -> f (nthArr arr (2*i)) (nthArr arr ((2*i)+1))) rango
+                     | otherwise = tabulateArr (\i -> if i == rango then nthArr arr (2*i) else f (nthArr arr (2*i)) (nthArr arr ((2*i)+1))) (rango+1)
+                     where rango = (div largo 2)
 
-reduceArr f e arr | len == 0  = e
-                  | len == 1  = f e (nthArr arr 0)
-                  | otherwise = let ctr = contraer f arr len
+reduceArr f e arr | largo == 0  = e
+                  | largo == 1  = f e (nthArr arr 0)
+                  | otherwise = let ctr = contraer f arr largo
                                 in reduceArr f e ctr
-                  where len = lengthArr arr
+                  where largo = lengthArr arr
+
+scanArr f e arr | largo == 0 = (emptyArr, e)
+                | largo == 1 = (singletonArr e, f e (nthArr arr 0))
+                | otherwise = let ctr = contraer f arr largo
+                                  (brr, r) = scanArr f e ctr
+                              in (expandirArr f arr brr, r)
+                where
+                  largo = lengthArr arr
+                  expandirArr f arr brr =  -- completar
