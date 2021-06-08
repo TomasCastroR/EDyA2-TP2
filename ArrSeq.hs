22c7b4ce1095d3418df8b8fcd3783fd7fd4d7f5f
Tomas Castro Rojas <tomascastrorojas18@gmail.com>
2021-06-08 17:05:11 -0300

    definiciones de las funciones alineados

--- ArrSeq.hs
+++ ArrSeq.hs
@@ -50,31 +50,31 @@ dropArr arr n = let largo = lengthArr arr
 
 showtArr arr | largo == 0 = EMPTY
              | largo == 1 = ELT (nthArr arr 0)
-             | otherwise = NODE (takeArr arr (div largo 2)) (dropArr arr (div largo 2))
+             | otherwise  = NODE (takeArr arr (div largo 2)) (dropArr arr (div largo 2))
              where largo = lengthArr arr
 
 showlArr arr  | largo == 0 = NIL
-              | otherwise = CONS (nthArr arr 0) (dropArr arr 1)
+              | otherwise  = CONS (nthArr arr 0) (dropArr arr 1)
               where largo = lengthArr arr
 
 joinArr = A.flatten
 
 contraerArr :: (a->a->a)-> A.Arr a -> Int -> A.Arr a
-contraerArr f arr largo | even largo  = tabulateArr (\i -> f (nthArr arr (2*i)) (nthArr arr ((2*i)+1))) rango
-                        | otherwise   = tabulateArr (\i -> if i == rango then nthArr arr (2*i) else f (nthArr arr (2*i)) (nthArr arr ((2*i)+1))) (rango+1)
+contraerArr f arr largo | even largo = tabulateArr (\i -> f (nthArr arr (2*i)) (nthArr arr ((2*i)+1))) rango
+                        | otherwise  = tabulateArr (\i -> if i == rango then nthArr arr (2*i) else f (nthArr arr (2*i)) (nthArr arr ((2*i)+1))) (rango+1)
                         where rango = (div largo 2)
 
 reduceArr f e arr | largo == 0  = e
                   | largo == 1  = f e (nthArr arr 0)
-                  | otherwise = let ctr = contraerArr f arr largo
-                                in reduceArr f e ctr
+                  | otherwise   = let ctr = contraerArr f arr largo
+                                  in reduceArr f e ctr
                   where largo = lengthArr arr
 
 scanArr f e arr | largo == 0 = (emptyArr, e)
                 | largo == 1 = (singletonArr e, f e (nthArr arr 0))
-                | otherwise = let ctr = contraerArr f arr largo
-                                  (brr, r) = scanArr f e ctr
-                              in (expandirArr f arr brr, r)
+                | otherwise  = let ctr = contraerArr f arr largo
+                                   (brr, r) = scanArr f e ctr
+                               in (expandirArr f arr brr, r)
                 where
                   largo = lengthArr arr
                   expandirArr f arr brr = tabulateArr (\i -> if even i then (nthArr brr (div i 2)) else f (nthArr brr (div i 2)) (nthArr arr (i - 1))) largo
--- ListSeq.hs
+++ ListSeq.hs
@@ -36,51 +36,51 @@ tabulateList f n = tabulateList' f n 0
                                         in x:xs
   
 
-mapList f [] = emptyList 
+mapList f []     = emptyList 
 mapList f (x:xs) = let (y, ys) = f x ||| mapList f xs
                    in y:ys
 
-filterList _ [] = emptyList 
+filterList _ []     = emptyList 
 filterList p (x:xs) = let (bool, ys) = p x ||| filterList p xs
                       in if bool then x : ys else ys 
 
-appendList [] sb = sb
-appendList sa [] = sa
+appendList [] sb     = sb
+appendList sa []     = sa
 appendList (a:sa) sb = a: (appendList sa sb)
 
 takeList s n = take n s
 
 dropList s n = drop n s
 
-showtList [] = EMPTY
+showtList []  = EMPTY
 showtList [x] = ELT x
-showtList xs = let largo = div (lenghtList xs) 2
-                   (l, r) = (takeList xs largo) ||| (dropList xs largo)
-               in NODE l r
+showtList xs  = let largo = div (lenghtList xs) 2
+                    (l, r) = (takeList xs largo) ||| (dropList xs largo)
+                in NODE l r
 
-showlList [] = NIL
+showlList []     = NIL
 showlList (x:xs) = CONS x xs
 
-joinList [] = emptyList 
+joinList []     = emptyList 
 joinList (x:xs) = appendList x (joinList xs)
 
 contraerList :: (a -> a -> a) -> [a] -> [a]
-contraerList _ [] = emptyList
-contraerList _ l@[x] = l
+contraerList _ []       = emptyList
+contraerList _ l@[x]    = l
 contraerList f (x:y:xs) = let (z,zs) = f x y ||| contraerList f xs
                           in z:zs
 
-reduceList _ e [] = e
+reduceList _ e []  = e
 reduceList f e [x] = f e x
-reduceList f e xs = reduceList f e (contraerList f xs)
+reduceList f e xs  = reduceList f e (contraerList f xs)
 
-scanList _ e [] = (emptyList , e)
+scanList _ e []  = (emptyList , e)
 scanList f e [x] = (singletonList e, f e x)
-scanList f e xs = let (ys, r) = scanList f e (contraerList f xs)
-                  in (expandirList f xs ys, r) 
-                where
-                  expandirList _ [] _ = []
-                  expandirList _ [_] ys = ys
-                  expandirList f (x:_:xs) (y:ys) = let (z, zs) = (f y x) ||| expandirList f xs ys in y:z:zs
+scanList f e xs  = let (ys, r) = scanList f e (contraerList f xs)
+                   in (expandirList f xs ys, r) 
+                  where
+                    expandirList _ [] _ = []
+                    expandirList _ [_] ys = ys
+                    expandirList f (x:_:xs) (y:ys) = let (z, zs) = (f y x) ||| expandirList f xs ys in y:z:zs
 
 fromList_ s = s 
